421b9d97b0fac679560cfc9a242045006841cf78
Josiah-MCS <josiah@microcoin.com>
2024-08-29 14:32:40 +1000

    added isGenuine to categorical vector

--- preprocessing/dataset.py
+++ preprocessing/dataset.py
@@ -51,7 +51,7 @@ class TraceDataset(Dataset):
         # Map the labels to numeric values
         df['labels_orientation'] = df['orientation'].map(self.orientation_to_index)
         df['labels_channel'] = df['channel'].map(self.channel_to_index)
-        self.labels = df[["labels_orientation", "labels_channel"]]
+        self.labels = df[["labels_orientation", "labels_channel", "isGenuine"]]
         ## put labels orientation and labels channel into a df with the same headers as before
 
     def fromOneHot(self, y):
@@ -60,9 +60,12 @@ class TraceDataset(Dataset):
             return index_to_value[index.numpy()].tolist()
         if len(y.shape) == 1:
             y = y.expand(1, -1)
-        y_orientation = _fromOneHot(y[:, 0:len(self.orientation)], self.orientation)
-        y_channel = _fromOneHot(y[:, 0:len(self.channel)], self.channel)
-        return y_orientation, y_channel
+
+        orientation_index, channel_index, isGenuine = np.split(y,[len(self.orientation), len(self.channel) + len(self.orientation)], axis=1)
+        
+        y_orientation = _fromOneHot(orientation_index, self.orientation)
+        y_channel = _fromOneHot(channel_index, self.channel)
+        return y_orientation, y_channel, np.squeeze(isGenuine.tolist())
     
     @classmethod
     def fromCsv(cls, csv_path: str, cache_pickle=True):
@@ -99,17 +102,20 @@ class TraceDataset(Dataset):
         len(self.orientation_to_index)
         y_orientation = toOneHot(y["labels_orientation"], len(self.orientation_to_index))
         y_channel = toOneHot(y["labels_channel"], len(self.channel_to_index))
-        y = np.concatenate([y_orientation, y_channel], axis=0)
+        y = [*y_orientation, *y_channel, y["isGenuine"].astype(int)]
         return torch.tensor(x, dtype=torch.float32), torch.tensor(y, dtype=torch.long)
 
 ##
-
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # trace_dataset = TraceDataset.fromCsv("assets/csv/ThailandV2.csv")
     # trace_dataset = TraceDataset.fromPickle("assets/csv/ThailandV2.csv.pkl")
     # trace_dataset.data
-    trace_dataset = TraceDataset.fromCsv("assets/csv/test.csv")
-    dataloader = DataLoader(trace_dataset, batch_size=32, shuffle=True)
-    i = iter(dataloader)
-    data = next(i)
-    trace_dataset.fromOneHot(data[1])
+
+trace_dataset = TraceDataset.fromCsv("assets/csv/test.csv")
+dataloader = DataLoader(trace_dataset, batch_size=32, shuffle=False)
+i = iter(dataloader)
+data = next(i)
+print(f"data[1] = {data[1]}")
+labels = trace_dataset.fromOneHot(data[1])
+print(f"labels = {labels}")
+
