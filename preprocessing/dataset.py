e85b2b96050f40653ffb6acf72050deec5fa632d
Josiah-MCS <josiah@microcoin.com>
2024-08-29 13:57:49 +1000

    added one hot encoding

--- preprocessing/dataset.py
+++ preprocessing/dataset.py
@@ -2,25 +2,25 @@ import pandas as pd
 import numpy as np
 import json
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import os
 
 
 dtype_dict = {
-    'path': str,
-    'country' : str,
-    'isGenuine' : bool, #bool,
-    'channel' : int,  #int,
-    'revision' : str,
-    'orientation' : str,
-    'validatorModel' : str,
-    'serial' : str,
-    'firmwareRevision' : str,
-    'created' : str,
-    'insertion' : int, #int
-    'sensor' : str,
-    'data' : str
-    }
+        'path': str,
+        'country' : str,
+        'isGenuine' : bool, #bool,
+        'channel' : int,  #int,
+        'revision' : str,
+        'orientation' : str,
+        'validatorModel' : str,
+        'serial' : str,
+        'firmwareRevision' : str,
+        'created' : str,
+        'insertion' : int, #int
+        'sensor' : str,
+        'data' : str
+        }
 
 def parse_dict(dict_str):
     try:
@@ -34,11 +34,36 @@ def parse_dict(dict_str):
 
 ##
 
+def toOneHot(y, num_classes):
+    """ 1-hot encodes a tensor """
+    return np.eye(num_classes, dtype='uint8')[y]
+
 class TraceDataset(Dataset):
     def __init__(self, df):
         self.data = df['data']
-        self.labels = df['channel']
+        self.orientation = df["orientation"].unique()
+        self.channel = df["channel"].unique()
 
+        # Create mappings from unique values to indices
+        self.orientation_to_index = {value: index for index, value in enumerate(self.orientation)}
+        self.channel_to_index = {value: index for index, value in enumerate(self.channel)}
+        
+        # Map the labels to numeric values
+        df['labels_orientation'] = df['orientation'].map(self.orientation_to_index)
+        df['labels_channel'] = df['channel'].map(self.channel_to_index)
+        self.labels = df[["labels_orientation", "labels_channel"]]
+        ## put labels orientation and labels channel into a df with the same headers as before
+
+    def fromOneHot(self, y):
+        def _fromOneHot(one_hot, index_to_value):
+            index = one_hot.argmax(axis=1)
+            return index_to_value[index.numpy()].tolist()
+        if len(y.shape) == 1:
+            y = y.expand(1, -1)
+        y_orientation = _fromOneHot(y[:, 0:len(self.orientation)], self.orientation)
+        y_channel = _fromOneHot(y[:, 0:len(self.channel)], self.channel)
+        return y_orientation, y_channel
+    
     @classmethod
     def fromCsv(cls, csv_path: str, cache_pickle=True):
         pickle_path = csv_path + '.pkl'
@@ -58,7 +83,7 @@ class TraceDataset(Dataset):
             df = pd.read_csv(csv_path, dtype=dtype_dict)
             df['data'] = df['data'].apply(parse_dict)
         return cls(df)
-    
+
     @classmethod
     def fromPickle(cls, pickle_path: str):
         df = pd.read_pickle(pickle_path)
@@ -71,9 +96,20 @@ class TraceDataset(Dataset):
         # x = self.data.iloc[idx][:,:200]
         x = self.data.iloc[idx]
         y = self.labels.iloc[idx]
+        len(self.orientation_to_index)
+        y_orientation = toOneHot(y["labels_orientation"], len(self.orientation_to_index))
+        y_channel = toOneHot(y["labels_channel"], len(self.channel_to_index))
+        y = np.concatenate([y_orientation, y_channel], axis=0)
         return torch.tensor(x, dtype=torch.float32), torch.tensor(y, dtype=torch.long)
+
+##
 
 if __name__ == "__main__":
     # trace_dataset = TraceDataset.fromCsv("assets/csv/ThailandV2.csv")
-    trace_dataset = TraceDataset.fromPickle("assets/csv/ThailandV2.csv.pkl")
-    trace_dataset.data
+    # trace_dataset = TraceDataset.fromPickle("assets/csv/ThailandV2.csv.pkl")
+    # trace_dataset.data
+    trace_dataset = TraceDataset.fromCsv("assets/csv/test.csv")
+    dataloader = DataLoader(trace_dataset, batch_size=32, shuffle=True)
+    i = iter(dataloader)
+    data = next(i)
+    trace_dataset.fromOneHot(data[1])
